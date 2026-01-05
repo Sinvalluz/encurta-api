6c4e85e2cdf6c4a2c76d860db492559f5436f8a9
Sinval Luz <contato.sinvalluz@gmail.com>
2026-01-05 09:21:33 -0300

    chore: initial structuring of the spring project

---
+++ src/main/java/com/SinvalDev/encurta_api/EncurtaApiApplication.java
@@ -0,0 +1,13 @@
+package com.SinvalDev.encurta_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SinvalDev/encurta_api/EncurtaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SinvalDev.encurta_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
