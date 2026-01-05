ddab1d9425e11e11b414217a816e4b9eca3db177
Sinval Luz <contato.sinvalluz@gmail.com>
2026-01-05 10:10:06 -0300

    feat: creates the Link entity in the database

---
+++ src/main/java/com/SinvalDev/encurta_api/model/Link.java
@@ -0,0 +1,31 @@
+package com.SinvalDev.encurta_api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Table(name = "link")
+public class Link {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "original_url", nullable = false)
+    private String originalUrl;
+
+    @Column(name = "short_code", unique = true, nullable = false)
+    private String shortCode;
+
+    @Column(name = "creation_date", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+}
