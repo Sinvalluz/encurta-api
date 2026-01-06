efa93eae452cf9bd787f9bcd03d407721c798cd0
Sinval Luz <contato.sinvalluz@gmail.com>
2026-01-06 08:15:17 -0300

    feat: introduces the logic for searching for the link using the shortcode

--- src/main/java/com/SinvalDev/encurta_api/repository/LinkRepository.java
+++ src/main/java/com/SinvalDev/encurta_api/repository/LinkRepository.java
@@ -3,5 +3,9 @@ package com.SinvalDev.encurta_api.repository;
 import com.SinvalDev.encurta_api.model.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LinkRepository extends JpaRepository<Link, Long> {
+
+    Optional<Link> findByShortCode(String shortCode);
 }
