bea6c5b752bb7b8eb52cb240a3a0eae0778f3079
Sinval Luz <contato.sinvalluz@gmail.com>
2026-02-02 19:26:39 -0300

    feat: add logging to HealthController

--- src/main/java/com/SinvalDev/encurta_api/controller/HealthController.java
+++ src/main/java/com/SinvalDev/encurta_api/controller/HealthController.java
@@ -1,11 +1,13 @@
 package com.SinvalDev.encurta_api.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/health")
@@ -13,6 +15,7 @@ public class HealthController {
 
     @GetMapping
     public ResponseEntity<String> health () {
+        log.info("Estou viva");
         return ResponseEntity.ok("Estou viva");
     }
 }
